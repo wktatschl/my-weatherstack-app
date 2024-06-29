bb5e281f6244e08305d3ead909f79cd5dc07165a
wolfBDX <wktatschl@icloud.com>
2024-06-30 00:22:38 +0200

    :sparkles: model, service and view created to make call to API and display info for searched city

---
+++ src/models/WeatherModel.ts
@@ -0,0 +1,7 @@
+export interface Weather {
+    cityName: string;
+    temperature: number;
+    description: string;
+    iconUrl?: string;
+    detailsUrl?: string;
+}
---
+++ src/services/WeatherService.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { Weather } from '../models/WeatherModel';
+
+// const API_KEY = process.env.VUE_APP_API_KEY;
+const BASE_URL = 'http://api.weatherstack.com/';
+
+export const fetchWeather = async (city: string): Promise<Weather> => {
+  try {
+    const response = await axios.get(`${BASE_URL}current`, {
+      params: {
+        access_key: 'e16a5c9f0b395804b5d0b33baa44e99e',
+        query: city,
+      },
+    });
+
+    const data = response.data;
+
+    if (!data || !data.current || !data.location || !data.request) {
+      throw new Error('Invalid response format');
+    }
+
+    return {
+      cityName: data.location.name,
+      temperature: data.current.temperature,
+      description: data.current.weather_descriptions[0] || 'Weather description not available',
+      iconUrl: data.current.weather_icons[0] || '',
+      detailsUrl: data.request.url || '',
+    };
+  } catch (error) {
+    console.error('Error fetching weather:', error);
+    throw new Error('Failed to fetch weather data');
+  }
+};
